3f5cf047f0204f55afb30522d821ec39f23aeab9
Khairul Ikhwan <khairul0355@hotmail.com>
2025-12-26 11:15:54 +0800

    removed google login

--- src/js/main.js
+++ src/js/main.js
@@ -32,36 +32,6 @@ const db = getDatabase();
 //     });
 // }
 
-///////////////////////////////////////AUTH//////////////////////////////////////////////////////
-
-const provider = new GoogleAuthProvider();
-
-const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user)
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    console.log(errorCode)
-    const errorMessage = error.message;
-    console.log(errorMessage)
-    // The email of the user's account used.
-    const email = error.customData.email;
-    console.log(email)
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log(credential)
-    // ...
-});
-
 //////////////////////////////////GET TEMPERATURE/////////////////////////////////////////
 
 const temperature_text = document.getElementById('temperature_text');
